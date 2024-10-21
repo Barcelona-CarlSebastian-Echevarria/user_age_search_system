0b17c269f3eb992fb9a8afbcdbd28caec76c2132
Carl <carlsebpogi123@gmail.com>
2024-10-21 12:37:42 +0800

    Trying some new approach: added a name function that allows multiple name input

---
+++ user_information_search_system.py
@@ -0,0 +1,39 @@
+# Ask the user for their name and age. Make room for added input categories if possible. Edit: Make sure that the first letter on each name parts are capitalized
+# Account for the error if the user failed to comply with thr prompt while maintaining the inputs already given
+# Store the information into an array for information logging. Each user must have their own storage
+# Give the user an option to add a category and a value if desired
+# Give the user the option to continue with the program or exit
+# The user names will be stored in an array as well so that the user can navigate to the different profiles already existed
+# Once the user decided to exit, display the profile of the oldest person logged
+
+
+
+# Allows multiple inputs to be included in the name 
+def get_user_name():
+    user_name = input("Enter your name: ")
+    user_name = user_name.split()
+
+    name = list(user_name)
+
+    try:
+        return (" ".join(name))
+
+    except:
+        return (user_name[0])
+
+
+
+name = get_user_name()
+print(name)
+
+
+
+
+
+
+
+
+    
+
+
+
