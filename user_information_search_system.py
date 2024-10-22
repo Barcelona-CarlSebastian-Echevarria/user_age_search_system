0b7de74abac2b129c4de104cd1cabdbe96d1e8e8
Carl <carlsebpogi123@gmail.com>
2024-10-22 13:14:58 +0800

    main function created; name function now capitalizes first letters

--- user_information_search_system.py
+++ user_information_search_system.py
@@ -8,18 +8,21 @@
 
 
 
-# Allows multiple inputs to be included in the name 
+# Allows multiple inputs to be included in the name
+# Converts the name to a list to accomodate multiple inputs, then make it a single word so that the .isalpha can be activated
 def get_user_name():
     while True:
         user_name = input("Enter your name: ")
         user_name = user_name.split()
         name = (''.join(user_name))
         if name.isalpha():
+            name = name.title()
+            # Makes the first letters of the words capitalized
             return name
         else:
             print("Enter a valid name")
 
-            
+# Asks for the user age      
 def get_user_age():
     while True:
        user_age = int(input("Enter your age: ")) 
@@ -28,9 +31,16 @@ def get_user_age():
        else:
           print("Please enter a valid age")
 
-name = get_user_name()
-age = get_user_age()
-print(name, age)
+# Sample function for dictionary storing
+user_dictionary = {}
+def main_system():
+    name = get_user_name()
+    age = get_user_age()
+    user_dictionary['name'] = name
+    user_dictionary['age'] = age
+    print(user_dictionary)
+
+main_system()
 
 
 
