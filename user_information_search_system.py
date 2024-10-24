9410f42b7d2fb0796a3fdb0bdcf6540b7eada268
Carl <carlsebpogi123@gmail.com>
2024-10-24 20:05:01 +0800

    Incorporated a library. The program now uploads the contents of all the infomation inputted to a csv file

--- user_information_search_system.py
+++ user_information_search_system.py
@@ -5,25 +5,29 @@
 # Give the user the option to continue with the program or exit
 # The usernames will be stored in an array as well so that the user can navigate to the different profiles already existed
 # Once the user decided to exit, display the profile of the oldest person logged
+# Edit: Add all the information inputted to a csv file
+import csv
 
 main_list = []
 number_list = []
 
 # Allows multiple inputs to be included in the name
-# Converts the name to a list to accommodate multiple inputs, then make it a single word so that the .isalpha can be activated
+# Converts the name to a list to accommodate multiple word inputs, then make it a single word so that the .isalpha can be activated
 def get_user_name():
     while True:
         user_name = input("Enter your name: ")
         user_name = user_name.split()
         name = (''.join(user_name))
-        if name.isalpha():
-            name = name.title()
+        letter_count = len(name)
+        if name.isalpha() and letter_count >= 2:
             # Makes the first letters of the words capitalized
+            name = name.title()
             return name
         else:
             print("Enter a valid name")
 
 # Asks for the user age
+# Will only limit the inputs to greater than zero since there are animals that can live for a thousand years
 def get_user_age():
     while True:
         try:
@@ -35,35 +39,47 @@ def get_user_age():
         except ValueError:
             print("Enter a numerical input only")
 
-# Create an individual user dictionary
+# Create an individual user dictionary and appends the user age to the number_list
 def create_user_dictionary():
     name_of_user = get_user_name()
     age_of_user = get_user_age()
     number_list.append(age_of_user)
-    user_dictionary = dict(name=name_of_user, age=age_of_user)
     # Creates a dictionary for the given inputs
+    user_dictionary = dict(name=name_of_user, age=age_of_user)
     return user_dictionary
 
 # Adds a new feature to the dictionary
 def add_dictionary_feature():
     user_dictionary = create_user_dictionary()
     while True:
-        new_feature = input("Do you want to add another feature to your dictionary (type 'y' or 'yes' to proceed, 'n' or 'no' to exit): ")
-        if new_feature.lower() == "y" or new_feature.lower() == "yes":
+        new_feature = input("Do you want to add another feature to your dictionary (type 'y' or 'yes' to proceed, 'n' or 'no' to exit): ").lower()
+        if new_feature == "y" or new_feature == "yes":
             while True:
-                dictionary_key = input("Enter a  key: ")
-                dictionary_value = input("Input the value ot the key you provided: ")
+                dictionary_key = input("Enter a key: ")
+                dictionary_value = input("Input the value of the key you provided: ")
                 user_dictionary.update({dictionary_key: dictionary_value})
                 break
-        elif new_feature.lower() == "n" or new_feature.lower() == "no":
+        elif new_feature == "n" or new_feature == "no":
             break
         else:
             print("Please respond with words/characters specified only")
 
     return user_dictionary
 
+# Uploads the contents in the main list to a csv file
+def csv_upload():
+    file_path = 'user_information_log.csv'
+    with open(file_path, 'w', newline='') as csvfile:
+        fieldnames = ['name', 'age']
+        writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
+        writer.writeheader()
+        writer.writerows(main_list)
+
+    return "Data uploaded to CSV File successfully"
+
 # Prints the users with oldest age and shows their profiles at user's choice
 def oldest_age_printer():
+    display_upload_successfully = csv_upload()
     profile_information_storage = []
     print(f"Profile with oldest age:")
     highest_age = max(number_list)
@@ -72,13 +88,15 @@ def oldest_age_printer():
             name = dictionaries["name"]
             age = dictionaries["age"]
             profile_information_storage.append(dictionaries)
-            print(f'name: {name}, age: {age}')
+            print(f'Name: {name}, Age: {age}')
+
+    print(display_upload_successfully)
 
     while True:
-        profile_viewing = input("View profile? (type 'y' or 'yes' to proceed, 'n' or 'no' to exit): ")
-        if profile_viewing.lower() == "y" or profile_viewing.lower() == "yes":
-                break
-        elif profile_viewing.lower() == "n" or profile_viewing.lower() == "no":
+        profile_viewing = input("View profile? (type 'y' or 'yes' to proceed, 'n' or 'no' to exit): ").lower()
+        if profile_viewing == "y" or profile_viewing == "yes":
+            break
+        elif profile_viewing == "n" or profile_viewing == "no":
             quit()
         else:
             print("Please respond using the specified")
@@ -89,11 +107,12 @@ def oldest_age_printer():
 # Adds new dictionary to the main list
 def edit_main_list():
     while True:
-        new_user_dictionary = input("Do you want to create a dictionary for another user (type 'y' or 'yes' to proceed, 'n' or 'no' to exit): ")
-        if new_user_dictionary.lower() == "y" or new_user_dictionary.lower() == "yes":
+        new_user_dictionary = input(
+            "Do you want to create a dictionary for another user (type 'y' or 'yes' to proceed, 'n' or 'no' to exit): ").lower()
+        if new_user_dictionary == "y" or new_user_dictionary == "yes":
             new = add_dictionary_feature()
             main_list.append(new)
-        elif new_user_dictionary.lower() == "n" or new_user_dictionary.lower() == "no":
+        elif new_user_dictionary == "n" or new_user_dictionary == "no":
             return None
         else:
             print("Enter an input using the characters/words provided")
@@ -103,26 +122,29 @@ def main_system():
     dictionary_created = add_dictionary_feature()
     main_list.append(dictionary_created)
     while True:
-        appeded_dictionary = edit_main_list()
-        if appeded_dictionary == None:
+        appended_dictionary = edit_main_list()
+        if appended_dictionary == None:
             break
 
     while True:
-        view_all_profiles = input("Before exiting, do you want to see all the profiles you've logged in?: ")
-        if view_all_profiles.lower() == "y" or view_all_profiles.lower() == "yes":
+        view_all_profiles = input(
+            "Before exiting, do you want to see all the profiles you've logged in? (type 'y' or 'yes' to proceed, 'n' or 'no' to exit): ").lower()
+        if view_all_profiles == "y" or view_all_profiles == "yes":
             print(main_list)
-            to_exit = input("Please press 'n' to exit or 'e' if you want to add a user profile (This is the last time you can edit): ")
-            if to_exit.lower() == 'e':
+            to_exit = input(
+                "Please press 'n' to exit or 'e' if you want to add a user profile (This is the last time you can edit): ")
+            if to_exit == 'e':
                 while True:
-                   final_edit_option = edit_main_list()
-                   if final_edit_option == None:
-                    break
-            elif to_exit.lower() == 'n':
+                    # Provide's the user with option to add a user dictionary while viewing all inputted information
+                    final_edit_option = edit_main_list()
+                    if final_edit_option == None:
+                        break
+            elif to_exit == 'n':
                 oldest_age_printer()
                 break
             else:
                 print("please respond using only what's specified")
-        elif view_all_profiles.lower() == "n" or view_all_profiles.lower() == "no":
+        elif view_all_profiles == "n" or view_all_profiles == "no":
             oldest_age_printer()
             break
         else:
